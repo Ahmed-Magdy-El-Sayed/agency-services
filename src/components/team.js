3063c4323ee58431c931fc4775ecfac3580a7edc
Ahmed-Magdy-El-Sayed <109208478+Ahmed-Magdy-El-Sayed@users.noreply.github.com>
2024-09-15 21:25:14 +0300

    Update team.js

--- src/components/team.js
+++ src/components/team.js
@@ -7,6 +7,8 @@ import user3 from "../images/user-3.jpg";
 import user4 from "../images/user-4.jpg";
 import { useEffect, useState } from "react";
 
+let intervalRunning = false;
+let centeringCard;
 export default function Team(){
     let data=[
         {h3:"Amazing App",img:user1,class:""},
@@ -53,38 +55,35 @@ export default function Team(){
         var cardsContainer = document.querySelector(".team-container .team");
         //form line 55 to 63 --> to make centerCard at the center of the page
         
+        if(intervalRunning) {
+            clearInterval(centeringCard);
+            intervalRunning = false;
+        }
         cardsContainer.style.left = 
-            cardsContainer.getBoundingClientRect().x < 0 ?
-                (- ( centerCard.getBoundingClientRect().x
-                + Math.abs(cardsContainer.getBoundingClientRect().x)
-                )) + window.innerWidth/2 - centerCard.offsetWidth/2+"px" 
-            :   (- ( centerCard.getBoundingClientRect().x
-                - cardsContainer.getBoundingClientRect().x
-                )) + window.innerWidth/2 - centerCard.offsetWidth/2 +"px" 
-            ;
-        
-                setTimeout(() => {
-            document.querySelector(".team-container").style.opacity=1;
-            for(var i = 0;i < cards.length; i++){
-                //for cards before center card
-                if(cards[i]!=centerCard && !centerFound){
-                    cards[i].style = null;
-                    cards[i].style.transform="rotateY(30deg) scale(0.8)";
-                    cards[i].style.transformOrigin= "right center";
-                }
-                //for cards after center card
-                else if(cards[i]!=centerCard && centerFound){
-                    cards[i].style = null;
-                    cards[i].style.transform="rotateY(-30deg) scale(0.8)";
-                    cards[i].style.transformOrigin= "left center";
-                }
-                //when reach to center card
-                else{
-                    cards[i].style = null;
-                    centerFound=true;
-                }
+            (
+                (cardsContainer.getBoundingClientRect().x - centerCard.getBoundingClientRect().x) 
+                + 
+                (window.innerWidth/2 - centerCard.offsetWidth/2)
+            ) + "px"
+        ;
+        intervalRunning = true;
+        centeringCard = setInterval(()=>{
+            if(Math.floor(centerCard.getBoundingClientRect().x) === Math.floor(window.innerWidth/2 - centerCard.offsetWidth/2)){
+                clearInterval(centeringCard)
+                intervalRunning = false;
+            }else{
+                cardsContainer.style.left = 
+                (
+                    (cardsContainer.getBoundingClientRect().x - centerCard.getBoundingClientRect().x) 
+                    + 
+                    (window.innerWidth/2 - centerCard.offsetWidth/2)
+                ) + "px"
+                ;
             }
-        }, 100);
+        },700)
+
+        if(intervalRunning)
+            return ()=>{clearInterval(centeringCard)}
     })
 
     return(
